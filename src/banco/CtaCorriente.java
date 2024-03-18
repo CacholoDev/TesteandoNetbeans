50e3fd92d8d72bd5bc8a2c9813989920b38c5602
adria <adria@DESKTOP-SNVUEVM>
2024-03-18 01:05:25 +0100

    first comm

---
+++ src/banco/CtaCorriente.java
@@ -0,0 +1,97 @@
+package banco;
+
+public class CtaCorriente {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cuenta
+     */
+    public String getCuenta() {
+        return cuenta;
+    }
+
+    /**
+     * @param cuenta the cuenta to set
+     */
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    /**
+     * @return the saldo
+     */
+    public double getSaldo() {
+        return saldo;
+    }
+
+    /**
+     * @param saldo the saldo to set
+     */
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    /**
+     * @return the interes
+     */
+    public double getInteres() {
+        return interes;
+    }
+
+    /**
+     * @param interes the interes to set
+     */
+    public void setInteres(double interes) {
+        this.interes = interes;
+    }
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double interes;
+
+    public CtaCorriente()
+    {
+    }
+    public CtaCorriente(String nombre, String cuenta, double saldo, double interes)
+    {
+        this.nombre = nombre;
+        this.cuenta = cuenta;
+        this.saldo = saldo;
+        this.interes = interes;
+    }
+    public double estado()
+    {
+        return getSaldo();
+    }
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retirar(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (estado()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+    
+    
+    
+}
